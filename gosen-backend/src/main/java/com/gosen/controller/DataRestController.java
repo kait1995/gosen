594c1accd84d668d3c94d5c7281431d36bc3714d
sekisei <25243356+sekisei@users.noreply.github.com>
2023-07-01 16:24:05 +0900

    Add a controller of MVC model for processing RESTful api.

---
+++ gosen-backend/src/main/java/com/gosen/controller/DataRestController.java
@@ -0,0 +1,17 @@
+package com.gosen.controller;
+
+public class DataRestController {
+	/*
+	 * [API]
+	 * /v1/system/add
+	 * /v1/system/delete/:id
+	 * /v1/system/update/:id
+	 * /v1/system/search/:title
+	 * /v1/system/info/:id/get
+	 * /v1/devices/:id/get/status
+	 * /v1/devices/:id/suspend
+	 * /v1/devices/:id/resume
+	*/
+	
+	
+}
