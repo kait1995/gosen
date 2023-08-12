fce414a8b0516600e25295cfb612003eb4aceb45
sekisei <25243356+sekisei@users.noreply.github.com>
2023-08-12 18:37:33 +0900

    Add: Error handling for same titled item requests.

--- gosen-backend/src/main/java/com/gosen/controller/DataRestController.java
+++ gosen-backend/src/main/java/com/gosen/controller/DataRestController.java
@@ -1,8 +1,14 @@
 package com.gosen.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.gosen.model.DeviceSetting;
 import com.gosen.repository.DeviceSettingRepository;
@@ -16,9 +22,10 @@ public class DataRestController {
 	 * /v1/system/update/:id
 	 * /v1/system/search/:title
 	 * /v1/system/info/:id/get
-	 * /v1/devices/:id/get/status
-	 * /v1/devices/:id/suspend
-	 * /v1/devices/:id/resume
+	 * /v1/device/:id/add
+	 * /v1/device/:id/get/status
+	 * /v1/device/:id/suspend
+	 * /v1/device/:id/resume
 	*/
 	
 	private final DeviceSettingRepository deviceSettingRepo;
@@ -28,9 +35,13 @@ public class DataRestController {
 	}
 	
 	@PostMapping(value="/v1/system/add", consumes="application/json;charset=UTF-8")
-	public String addItem(@RequestBody DeviceSetting deviceSetting) {
-		deviceSettingRepo.save(deviceSetting);
-		return "Test OK";
+	public ResponseEntity<String> addItem(@RequestBody DeviceSetting deviceSetting) {
+		if(deviceSettingRepo.existsByTitle(deviceSetting.getTitle())) {
+			return new ResponseEntity<>("The title is already in use.", HttpStatus.BAD_REQUEST);
+		} else {
+			deviceSettingRepo.save(deviceSetting);
+			return ResponseEntity.ok("Response ok.");
+		}
 	}
 	
 }
