c540c05cf7e277ae87c2c1f3c8587bb16413da65
sekisei <25243356+sekisei@users.noreply.github.com>
2023-12-09 22:39:13 +0900

    Change the project name

---
+++ gosen-api/src/main/java/com/gosen/GosenApiApplication.java
@@ -0,0 +1,13 @@
+package com.gosen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GosenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GosenApiApplication.class, args);
+	}
+
+}
