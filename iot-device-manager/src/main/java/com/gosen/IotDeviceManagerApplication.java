30cf48678a45c004f9be2d4123f9fd181937a709
sekisei <25243356+sekisei@users.noreply.github.com>
2023-12-11 21:49:13 +0900

    Restore commit files

---
+++ iot-device-manager/src/main/java/com/gosen/IotDeviceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.gosen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotDeviceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotDeviceManagerApplication.class, args);
+	}
+
+}
