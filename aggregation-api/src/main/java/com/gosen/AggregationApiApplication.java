f36cde95a54ae4d957b8176aca5a2984e05c3142
sekisei <25243356+sekisei@users.noreply.github.com>
2023-12-12 10:26:18 +0900

    Add a backend for frontend

---
+++ aggregation-api/src/main/java/com/gosen/AggregationApiApplication.java
@@ -0,0 +1,13 @@
+package com.gosen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggregationApiApplication.class, args);
+	}
+
+}
